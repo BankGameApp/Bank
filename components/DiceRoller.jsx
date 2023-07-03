f2575da8dbf9692327a72862d8f6557c62b20ec9
Higgins723 <xxhiggins723xx@gmail.com>
2023-07-03 01:01:31 -0600

    update dice roller component again

--- components/DiceRoller.jsx
+++ components/DiceRoller.jsx
@@ -7,19 +7,16 @@ const DiceRoller = ({ setScore }) => {
   const [dice2, setDice2] = useState(null);
   const [rolling, setRolling] = useState(false);
 
-  const generateRandomDiceArrays = async () => {
+  const generateRandomNumber = () => {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
+  const generateRandomDiceSet = async () => {
     setRolling(true);
 
-    for (let i = 0; i < 10; i++) {
-      let num1, num2;
-
-      do {
-        num1 = Math.floor(Math.random() * 6) + 1;
-      } while (i > 0 && num1 === dice1);
-
-      do {
-        num2 = Math.floor(Math.random() * 6) + 1;
-      } while (i > 0 && num2 === dice2);
+    for (let i = 0; i < 10; i += 1) {
+      const num1 = generateRandomNumber()
+      const num2 = generateRandomNumber()
 
       setDice1(num1);
       setDice2(num2);
@@ -31,7 +28,7 @@ const DiceRoller = ({ setScore }) => {
     setRolling(false);
   };
 
-  const returnDiceImg = (diceValue) => {
+  const returnDiceSrc = (diceValue) => {
     const color = colorMode === 'dark' ? 'Light' : 'Dark';
     const diceImage = `/Dice/${diceValue}Dice${color}.png`;
 
@@ -57,6 +54,16 @@ const DiceRoller = ({ setScore }) => {
     };
   }, [rolling, dice1, dice2])
 
+  const returnDiceImg = (dice) => {
+    if (!dice) return
+
+    return (
+      <Flex alignItems='center' justifyContent='center' margin={2}>
+        <img src={returnDiceSrc(dice)} width={100} height={100} />
+      </Flex>
+    )
+  }
+
   return (
     <Flex
       alignItems='center'
@@ -65,7 +72,7 @@ const DiceRoller = ({ setScore }) => {
     >
       <Button
         isDisabled={rolling}
-        onClick={generateRandomDiceArrays}
+        onClick={generateRandomDiceSet}
         marginBottom={2}
       >
         {rolling ? 'Rolling...' : 'Roll Dice'}
@@ -75,12 +82,8 @@ const DiceRoller = ({ setScore }) => {
         gap={4}
         justifyContent='center'
       >
-        <Flex alignItems='center' justifyContent='center' margin={2}>
-          {dice1 && <img src={returnDiceImg(dice1)} width={100} height={100} />}
-        </Flex>
-        <Flex alignItems='center' justifyContent='center' margin={2}>
-          {dice2 && <img src={returnDiceImg(dice2)} width={100} height={100} />}
-        </Flex>
+        {returnDiceImg(dice1)}
+        {returnDiceImg(dice2)}
       </Grid>
     </Flex>
   );
